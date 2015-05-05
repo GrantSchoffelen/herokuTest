ccfaf35cd85c7c9213d80fdc6aecd4450f48e3e1
GrantSchoffelen <grant.schoffelen@gmail.com>
2015-05-04 22:37:17 -0500

    2015, 04, 05

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var fs = require('fs');
+var exec = require('exec-then');
+var schedule = require('node-schedule');
+var moment = require('moment');
 
 var app = express();
 
@@ -55,6 +59,54 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
+
+
+
+var today = moment();
+var setDate = moment(); 
+
+var j = schedule.scheduleJob(today.format(), function(){
+   dayCommit(today)
+      
+});
+
+function dayCommit(date){ 
+
+  fs.writeFile("newfile.js", date.format("YYYY, DD, MM"), function(err) {
+    if(err) {
+      console.log('err1')
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+
+
+}); 
+exec('git add -A',
+  function (error, stdout, stderr) {
+    console.log('add')
+    if (error !== null) {
+      console.log('err2')
+      console.log('exec error: ' + error);
+    }
+}).then(function(){
+      exec("git commit --date='"+date.format("YYYY, DD, MM")+"' -m'"+date.format("YYYY, DD, MM")+"'",
+  function (error, stdout, stderr) {
+    console.log('commit')
+
+    if (error !== null) {
+      console.log('exec error: ' + error);
+      console.log('err3')
+    }
+}).then(function(){
+   exec('git push origin master', function(error, stdout, stderr){
+      console.log('push')
+    })
+});
+
+});
+
+}
 
 
 module.exports = app;
